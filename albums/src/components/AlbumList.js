de940fb396e39d5a110d30ee82af4011eaa2147f
Guðný Björk Gunnarsdóttir <gudnybg@Gudnys-MacBook-Pro.local>
2016-11-29 16:25:06 +0000

    ajax call for music albums returning in console log

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
   componentWillMount() {
-    console.log('componentWillMount in AlbumList');
+    //console.log('componentWillMount in AlbumList');
+    //debugger; þarf að sækja einhvern package til að nota í crome.
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => console.log(response));
   }
 
   render() {
