82c593a6c0b921ec7baf1c566377c40d85613b83
Guðný Björk Gunnarsdóttir <gudnybg@Gudnys-MacBook-Pro.local>
2016-11-29 16:01:25 +0000

    fix components file mixup

--- albums/src/components/Header.js
+++ albums/src/components/Header.js
@@ -3,24 +3,33 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 //Make a component
-const Header = () => {
+const Header = (props) => {
   const { textStyle, viewStyle } = styles;
 
   return (
     <View style={viewStyle}>
-      <Text style={textStyle}>Albums!</Text>
+      <Text style={textStyle}>props.headerText</Text>
     </View>
   );
 };
 
 const styles = {
-  viewStyle : {
+  viewStyle: {
     backgroundColor: '#F8F8F8',
     justifyContent: 'center',
     alignItems: 'center',
-
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
   },
-  textStyle : {
-    
+  textStyle: {
+    fontSize: 20
   }
-}
+};
+
+// Make component availabele to other parts of the app
+export default Header;
