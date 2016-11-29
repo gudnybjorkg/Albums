1084bad4e10a6d6841cf300beae991bb19d94213
Guðný Björk Gunnarsdóttir <gudnybg@Gudnys-MacBook-Pro.local>
2016-11-29 15:53:19 +0000

    styling

--- albums/src/componets/Header.js
+++ albums/src/componets/Header.js
@@ -14,13 +14,20 @@ const Header = (props) => {
 };
 
 const styles = {
-  viewStyle : {
+  viewStyle: {
     backgroundColor: '#F8F8F8',
     justifyContent: 'center',
     alignItems: 'center',
-
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2
   },
-  textStyle : {
-
+  textStyle: {
+    fontSize: 20
   }
-}
+};
+
+// Make component availabele to other parts of the app
+export default Header;
