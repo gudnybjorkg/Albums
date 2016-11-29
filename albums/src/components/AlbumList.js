15a6b199a572d870ad0c3d6bdfbc0b5972097320
Guðný Björk Gunnarsdóttir <gudnybg@Gudnys-MacBook-Pro.local>
2016-11-29 16:44:14 +0000

    gets data from rallycoding.herukuapp.com

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -3,13 +3,29 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  // (1) Setup default initial state (empty state)
+  state = { albums: [] };
+
   componentWillMount() {
     //console.log('componentWillMount in AlbumList');
     //debugger; þarf að sækja einhvern package til að nota í crome.
-    axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => console.log(response));
+
+    // Get the data
+    // axios.get('https://rallycoding.herokuapp.com/api/music_albums').then(response => console.log(response));
+    // Make sure the data has been returned before rendering. Using component level state
+    // Using state = { albums: [ {name: 'Taylor Swift'}, ...] }.
+    // 1. setup default state 2. tell comonent to update 3. make sure that the state is used
+
+    //(2)
+    // setstate: til í öllum class based components og rerenderar fyrir mann.
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
   }
 
   render() {
+    //renderar fyrst tóma og svo með gögnum vegna setState
+    console.log(this.state);
+
     return (
       <View>
         <Text> AlbumList!!!!</Text>
