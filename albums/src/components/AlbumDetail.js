31a285ea1e1f50a5fb676214197d0f248ecad5ac
Guðný Björk Gunnarsdóttir <gudnybg@Gudnys-MacBook-Pro.local>
2016-11-30 16:21:05 +0000

    bæta við mynd og style

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
 //Þar sem þetta er bara presentational component þá þarf hann ekki að vera með state
 //hann þarf því ekki að vera class og er því functional component
-const AlbumDetail = (props) => {
+//í stað þess að vera bara með prop þá er hægt að destructura með því að
+//sækja beint það sem verið að nota, þá er hægt að gera const breytu og pikka út það
+//sem þarf til að nota hverju sinni í stað þess að skrifa alla gommuna
+//props.album.title og kalla því beint í title.
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+
   return (
     <Card>
       <CardSection>
         <View style={styles.headerContentStyle}>
-
+          <Image
+            style={styles.thumbnailStyle}
+            source={{ uri: thumbnail_image }}
+          />
         </View>
 
         <View>
-          <Text>{props.album.title}</Text>
-          <Text>{props.album.artist}</Text>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
         </View>
       </CardSection>
     </Card>
@@ -26,6 +35,10 @@ const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
   }
 };
 
