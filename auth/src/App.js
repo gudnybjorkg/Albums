4f2cd5d95286726e142d3c8b226c3a124d0022a2
Guðný Björk Gunnarsdóttir <gudnybg@Gudnys-MacBook-Pro.local>
2016-12-02 15:23:10 +0000

    firebase added

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './Components/Common';
 
 class App extends Component {
+  // kallast automatískt í það ef það er skilgreint hér.
+  componentWillMount() {
+    firebase.initislizeApp({
+      apiKey: 'AIzaSyAZIDxuXhvo3bvDplQVErcYYTJd42AUtxA',
+      authDomain: 'authentication-13509.firebaseapp.com',
+      databaseURL: 'https://authentication-13509.firebaseio.com',
+      storageBucket: 'authentication-13509.appspot.com',
+      messagingSenderId: '599250066361'
+    });
+  }
   render() {
     return (
         <View>
