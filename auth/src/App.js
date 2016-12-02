c4d2b0625f0e54eadebabb829bee4e298ddef001
Guðný Björk Gunnarsdóttir <gudnybg@Gudnys-MacBook-Pro.local>
2016-12-02 15:38:47 +0000

    loginform

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './Components/Common';
+import LoginForm from './Components/LoginForm';
 
 class App extends Component {
   // kallast automatískt í það ef það er skilgreint hér.
   componentWillMount() {
-    firebase.initislizeApp({
+    firebase.initializeApp({
       apiKey: 'AIzaSyAZIDxuXhvo3bvDplQVErcYYTJd42AUtxA',
       authDomain: 'authentication-13509.firebaseapp.com',
       databaseURL: 'https://authentication-13509.firebaseio.com',
@@ -14,11 +15,12 @@ class App extends Component {
       messagingSenderId: '599250066361'
     });
   }
+
   render() {
     return (
         <View>
           <Header headerText="Authentication" />
-          <Text>An App!</Text>
+          <LoginForm />
         </View>
     );
   }
---
+++ auth/src/Components/LoginForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Button, Card, CardSection } from './Common';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      < View />
+    );
+  }
+}
+
+export default LoginForm;
