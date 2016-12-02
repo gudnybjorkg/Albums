909dc0f6631ee95f4394d01c4b67cc7514f1e67f
Guðný Björk Gunnarsdóttir <gudnybg@Gudnys-MacBook-Pro.local>
2016-12-02 14:26:50 +0000

    fix glósur og import

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './Componentst/Common';
+import { Header } from './Components/Common';
 
 class App extends Component {
   render() {
