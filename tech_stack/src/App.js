b0438b092b7d4cb356e28749d34c8f14425f8172
Guðný Björk Gunnarsdóttir <gudnybg@Gudnys-MacBook-Pro.local>
2016-12-06 15:10:21 +0000

    add provider and store

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
+// heldur utan um store-ið og sér um að translate-a það sem er í storeinu fyrir appið
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'; // býr til storeið
 
-const App ) () = {
+const App = () => {
   return (
-    <View />
+    <Provider store={createStore}>
+      <View />
+    </Provider>
   );
 };
 
