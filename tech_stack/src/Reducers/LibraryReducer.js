116a3ce6719db6a989f55e9f84e116247cc324ab
Guðný Björk Gunnarsdóttir <gudnybg@Gudnys-MacBook-Pro.local>
2016-12-06 15:52:27 +0000

    listi af tölvuleikjum eru nú skilarðir úr libarayreducer og add librarylist

--- tech_stack/src/Reducers/LibraryReducer.js
+++ tech_stack/src/Reducers/LibraryReducer.js
@@ -1,3 +1,5 @@
-export default () => [
-  
-];
+import data from './LibraryList.json';
+
+//alltaf þegar kallað er í LibraryReducer þá skilast þessi gögn
+// getState skilar þá { libraries: [{ id: 0, title: Zelda...}]}
+export default () => data;
