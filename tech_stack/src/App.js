ba079ccd09876010e6147f65dc8c90fd2a2d5d95
Guðný Björk Gunnarsdóttir <gudnybg@Gudnys-MacBook-Pro.local>
2016-12-06 15:13:53 +0000

    add reducer

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -3,10 +3,11 @@ import { View } from 'react-native';
 // heldur utan um store-ið og sér um að translate-a það sem er í storeinu fyrir appið
 import { Provider } from 'react-redux';
 import { createStore } from 'redux'; // býr til storeið
+import Reducers from './Reducers';
 
 const App = () => {
   return (
-    <Provider store={createStore}>
+    <Provider store={createStore(Reducers)}>
       <View />
     </Provider>
   );
---
+++ tech_stack/src/Reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+//nota combine reducers til að láta alla reducerana leika fallega saman
+export default combineReducers({
+  libraries: () => []
+})
