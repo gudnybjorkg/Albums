c2cc357bb353e23a4e1478b40b43b1fc9f5af685
Guðný Björk Gunnarsdóttir <gudnybg@Gudnys-MacBook-Pro.local>
2016-12-06 15:46:39 +0000

    add libraryList json with list of computer games

--- tech_stack/src/Reducers/index.js
+++ tech_stack/src/Reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
+import LibraryReducer from './LibraryReducer';
 
 //nota combine reducers til að láta alla reducerana leika fallega saman
+//T.d. hér er listi af libraries sem á að birta, tvö state til að halda utan um
+//valið library sem á að expanda og svo lista af libraries.
+//console.log(store.getState()); skilar {libraries: []} því LibraryReducer skilar tómu []
 export default combineReducers({
-  libraries: () => []
-})
+  libraries: LibraryReducer
+});
