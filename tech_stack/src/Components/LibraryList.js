fbb5281719ea8305469960800371f09577dac052
Guðný Björk Gunnarsdóttir <gudnybg@Gudnys-MacBook-Pro.local>
2016-12-06 22:34:15 +0000

    listview bætt við

--- tech_stack/src/Components/LibraryList.js
+++ tech_stack/src/Components/LibraryList.js
@@ -1,18 +1,41 @@
 //render the list of libaries to the user
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { ListView } from 'react-native';
 
 //sækja gögn í state og birta með redux
 class LibraryList extends Component {
+  componentWillMount() {
+    //Gögnin sem á að birta í listviewinu
+    const ds = new ListView.dataSource({
+      //?
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    //datasource for the listview
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  //Segir hvernig á að rendera eina row i listanum
+  renderRow() {
+
+  }
+
   render() {
-    return;
+    //console.log(this.props);
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
   }
 }
 
 //Takes the global state object (state) og mappar það (tekur property í stateinu)
 //provides them as props to the compontes.
 const mapStateToProps = state => {
-  console.log(state);
+  return { libraries: state.libraries };
 };
 
 //Nota connect()(): kallar í connect þá skilar það öðru falli í þessu tilfelli
